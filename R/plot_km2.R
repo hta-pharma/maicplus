7b99761f646de02e7b496eafc9347b8e985e0e91
Michael Seo <swj8874@gmail.com>
2024-02-01 23:44:43 +0100

    rev

--- R/plot_km2.R
+++ R/plot_km2.R
@@ -24,6 +24,8 @@
 #'   \item if "by_arm", 2 by 1 plot, first KM curves of \code{trt_agd} and  \code{trt_ipd} (with and without weights), and then KM cuvers of \code{trt_common} in AgD trial and IPD trial (with and without weights). Risk set table is appended.
 #'   \item if "all", 2 by 2 plot, all plots in "by_trial" and "by_arm" without risk set table appended.
 #' }
+#' @example inst/examples/kmplot2_anchored_ex.R
+#' @example inst/examples/kmplot2_unanchored_ex.R
 #' @export
 
 kmplot2 <- function(weights_object,
