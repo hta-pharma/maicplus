45cbd9a36b50fe758adbcc2833ba884cd01cc41d
Gregory Chen <stat1013@gmail.com>
2024-05-02 20:35:52 +0000

    update unanchor binary example annotation

--- inst/examples/maic_unanchored_binary_ex.R
+++ inst/examples/maic_unanchored_binary_ex.R
@@ -1,16 +1,17 @@
-# simulate data to demo binary maic
+# load in prognostic IPD data and AgD
 devtools::load_all()
 load(system.file("extdata", "ipd.rda", package = "maicplus", mustWork = TRUE))
 load(system.file("extdata", "agd.rda", package = "maicplus", mustWork = TRUE))
 ipd_centered <- center_ipd(ipd = ipd, agd = agd)
 
+# estimate weights
 centered_colnames <- c("AGE", "AGE_SQUARED", "SEX_MALE", "ECOG0", "SMOKE", "N_PR_THER_MEDIAN")
 centered_colnames <- paste0(centered_colnames, "_CENTERED")
+
 weighted_data <- estimate_weights(data = ipd_centered, centered_colnames = centered_colnames)
 weighted_data2 <- estimate_weights(data = ipd_centered, centered_colnames = centered_colnames, n_boot_iteration = 500)
-check <- weighted_data2$boot
 
-
+# get dummy binary IPD
 adrs <- read.csv(system.file("extdata", "adrs.csv", package = "maicplus", mustWork = TRUE))
 adrs$RESPONSE <- adrs$AVAL
 
@@ -19,7 +20,7 @@ pseudo_adrs <- get_pseudo_ipd_binary(binary_agd = data.frame(ARM = rep("B",2),
                                                              COUNT = c(280,120)),
                                      format = "stacked")
 
-
+# unanchored binary MAIC, with CI based on sandwich estimator
 maic_unanchored(weights_object = weighted_data,
                 ipd = adrs,
                 pseudo_ipd = pseudo_adrs,
@@ -33,7 +34,7 @@ maic_unanchored(weights_object = weighted_data,
                 # binary specific args
                 binary_robust_cov_type = "CR2")
 
-
+# unanchored binary MAIC, with bootstrapped CI
 maic_unanchored(weights_object = weighted_data2,
                 ipd = adrs,
                 pseudo_ipd = pseudo_adrs,
