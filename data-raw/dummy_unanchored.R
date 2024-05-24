b60cea7b68012b1cee4f5446ee7f7d5894c74972
Gregory Chen <stat1013@gmail.com>
2024-05-24 07:02:31 +0000

    add unanchored data

---
+++ data-raw/dummy_unanchored.R
@@ -0,0 +1,43 @@
+#### create datasets for unanchored case ####
+## adsl_sat, adtte_sat, adrs_sat, agd_sat (AgD of effect modifiers), pseudo_ipd_sat (AgD, tte data)
+
+devtools::load_all()
+# Read in relevant ADaM data and rename variables of interest
+adsl_sat <- read.csv(system.file("extdata", "adsl.csv",
+  package = "maicplus",
+  mustWork = TRUE
+))
+adtte_sat <- read.csv(system.file("extdata", "adtte.csv",
+  package = "maicplus",
+  mustWork = TRUE
+))
+adtte_sat$TIME <- adtte_sat$AVAL
+adtte_sat$EVENT <- adtte_sat$EVNT
+adtte_sat <- adtte_sat[adtte_sat$ARM == "A", , drop = FALSE]
+adsl_sat <- adsl_sat[adsl_sat$USUBJID %in% adtte_sat$USUBJID, , drop = FALSE]
+
+### AgD
+# Baseline aggregate data for the comparator population
+agd_sat <- read.csv(system.file("extdata", "aggregate_data_example_1.csv",
+  package = "maicplus", mustWork = TRUE
+))
+# for time-to-event endpoints, pseudo IPD from digitalized KM
+pseudo_ipd_sat <- read.csv(system.file("extdata", "psuedo_IPD.csv",
+  package = "maicplus",
+  mustWork = TRUE
+))
+pseudo_ipd_sat$ARM <- "B"
+
+### Centered IPD
+agd_sat <- process_agd(agd_sat)
+adsl_sat <- dummize_ipd(adsl_sat, dummize_cols = c("SEX"), dummize_ref_level = c("Female"))
+centered_ipd_sat <- center_ipd(ipd = adsl_sat, agd = agd_sat)
+
+### Binary
+adrs_sat <- read.csv(system.file("extdata", "adrs.csv", package = "maicplus", mustWork = TRUE))
+adrs_sat$RESPONSE <- adrs_sat$AVAL
+
+### Output
+usethis::use_data(adsl_sat, adtte_sat, agd_sat, pseudo_ipd_sat, centered_ipd_sat, adrs_sat,
+  internal = FALSE, overwrite = TRUE
+)
