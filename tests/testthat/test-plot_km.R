c9a16f986d699ceb56d35dd9cbb2b50900f68b67
Isaac Gravestock <isaac.gravestock@roche.com>
2024-07-18 12:38:19 +0200

    more snapshots

--- tests/testthat/test-plot_km.R
+++ tests/testthat/test-plot_km.R
@@ -2,13 +2,27 @@ test_that("ph_diagplot_lch works without error", {
   original_par <- par()
   load(system.file("extdata", "combined_data_tte.rda", package = "maicplus", mustWork = TRUE))
   kmobj <- survfit(Surv(TIME, EVENT) ~ ARM, combined_data_tte, conf.type = "log-log")
-  ph_diagplot_lch(
-    kmobj,
-    time_scale = "month", log_time = TRUE,
-    endpoint_name = "OS", subtitle = "(Before Matching)"
+  expect_no_error(
+    ph_diagplot_lch(
+      kmobj,
+      time_scale = "month", log_time = TRUE,
+      endpoint_name = "OS", subtitle = "(Before Matching)"
+    )
   )
 
   expect_equal(original_par, par())
+
+  skip_if_not_installed("vdiffr")
+  vdiffr::expect_doppelganger(
+    title = "ph_diagplot_lch",
+    fig = function() {
+      ph_diagplot_lch(
+        kmobj,
+        time_scale = "month", log_time = TRUE,
+        endpoint_name = "OS", subtitle = "(Before Matching)"
+      )
+    }
+  )
 })
 
 test_that("ph_diagplot_schoenfeld works without error", {
@@ -23,32 +37,18 @@ test_that("ph_diagplot_schoenfeld works without error", {
     )
   )
   expect_equal(original_par, par())
-})
 
-test_that("ph_diagplot_lch works without error", {
-  original_par <- par()
-  load(system.file("extdata", "combined_data_tte.rda", package = "maicplus", mustWork = TRUE))
-  kmobj <- survfit(Surv(TIME, EVENT) ~ ARM, combined_data_tte, conf.type = "log-log")
-  ph_diagplot_lch(
-    kmobj,
-    time_scale = "month", log_time = TRUE,
-    endpoint_name = "OS", subtitle = "(Before Matching)"
+  skip_if_not_installed("vdiffr")
+  vdiffr::expect_doppelganger(
+    title = "ph_diagplot_schoenfeld",
+    fig = function() {
+      ph_diagplot_schoenfeld(
+        unweighted_cox,
+        time_scale = "month", log_time = TRUE,
+        endpoint_name = "OS", subtitle = "(Before Matching)"
+      )
+    }
   )
-
-  expect_equal(original_par, par())
-})
-
-test_that("ph_diagplot_schoenfeld works without error", {
-  original_par <- par()
-  load(system.file("extdata", "combined_data_tte.rda", package = "maicplus", mustWork = TRUE))
-  unweighted_cox <- coxph(Surv(TIME, EVENT == 1) ~ ARM, data = combined_data_tte)
-  expect_no_error(
-    ph_diagplot_schoenfeld(unweighted_cox,
-      time_scale = "month", log_time = TRUE,
-      endpoint_name = "OS", subtitle = "(Before Matching)"
-    )
-  )
-  expect_equal(original_par, par())
 })
 
 test_that("kmplot2 works", {
