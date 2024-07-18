f7093bc14197dbfc0eb4e50779c9a3f0b51128bc
Isaac Gravestock <isaac.gravestock@roche.com>
2024-07-18 12:33:10 +0200

    restyle

--- tests/testthat/test-plot_km.R
+++ tests/testthat/test-plot_km.R
@@ -2,7 +2,8 @@ test_that("ph_diagplot_lch works without error", {
   original_par <- par()
   load(system.file("extdata", "combined_data_tte.rda", package = "maicplus", mustWork = TRUE))
   kmobj <- survfit(Surv(TIME, EVENT) ~ ARM, combined_data_tte, conf.type = "log-log")
-  ph_diagplot_lch(kmobj,
+  ph_diagplot_lch(
+    kmobj,
     time_scale = "month", log_time = TRUE,
     endpoint_name = "OS", subtitle = "(Before Matching)"
   )
@@ -15,7 +16,8 @@ test_that("ph_diagplot_schoenfeld works without error", {
   load(system.file("extdata", "combined_data_tte.rda", package = "maicplus", mustWork = TRUE))
   unweighted_cox <- coxph(Surv(TIME, EVENT == 1) ~ ARM, data = combined_data_tte)
   expect_no_error(
-    ph_diagplot_schoenfeld(unweighted_cox,
+    ph_diagplot_schoenfeld(
+      unweighted_cox,
       time_scale = "month", log_time = TRUE,
       endpoint_name = "OS", subtitle = "(Before Matching)"
     )
@@ -27,7 +29,8 @@ test_that("ph_diagplot_lch works without error", {
   original_par <- par()
   load(system.file("extdata", "combined_data_tte.rda", package = "maicplus", mustWork = TRUE))
   kmobj <- survfit(Surv(TIME, EVENT) ~ ARM, combined_data_tte, conf.type = "log-log")
-  ph_diagplot_lch(kmobj,
+  ph_diagplot_lch(
+    kmobj,
     time_scale = "month", log_time = TRUE,
     endpoint_name = "OS", subtitle = "(Before Matching)"
   )
