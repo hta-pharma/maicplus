d35803d6e5fa538d349212d535faf12b48dbef12
Isaac Gravestock <isaac.gravestock@roche.com>
2024-07-19 14:22:30 +0200

    improve plots

--- R/time-helper.R
+++ R/time-helper.R
@@ -3,6 +3,7 @@ settings_env <- new.env()
 
 #' Get and Set Time Conversion Factors
 #'
+#' @param default The default time scale, commonly whichever has factor = 1
 #' @param days Factor to divide data time units to get time in days
 #' @param weeks Factor to divide data time units to get time in weeks
 #' @param months Factor to divide data time units to get time in months
@@ -13,13 +14,33 @@ settings_env <- new.env()
 #' @rdname time_conversion
 #'
 #' @examples
-#' # Native time format is years
-#' set_time_conversion(days = 1 / 365.25, weeks = 1 / 52.17857, months = 1 / 12, years = 1)
+#' # The default time scale is days:
+#' set_time_conversion(default = "days", days = 1, weeks = 7, months = 365.25 / 12, years = 365.25)
 #'
-#' # Native time format is days
-#' set_time_conversion(days = 1, weeks = 7, months = 365.25 / 12, years = 365.25)
-set_time_conversion <- function(days = 1, weeks = 7, months = 365.25 / 12, years = 365.25) {
-  settings_env$time_conversion <- c(days = days, weeks = weeks, months = months, years = years)
+#'
+#' # Set the default time scale to years
+#' set_time_conversion(
+#'   default = "years",
+#'   days = 1 / 365.25,
+#'   weeks = 1 / 52.17857,
+#'   months = 1 / 12,
+#'   years = 1
+#' )
+#'
+set_time_conversion <- function(default = "days", days = 1, weeks = 7, months = 365.25 / 12, years = 365.25) {
+  if (!default %in% c("days", "weeks", "months", "years")) {
+    stop("default must be one of \"days\", \"weeks\", \"months\", \"years\")")
+  }
+  factors <- c(days = days, weeks = weeks, months = months, years = years)
+  check_factors <- vapply(factors, function(x) isFALSE(!is.finite(x) || x == 0), logical(1L))
+  if (!all(check_factors)) {
+    stop(
+      "Conversion factors must be finite non-zero numerical values: ",
+      paste0(names(factors)[!check_factors], " = ", factors[!check_factors], collapse = ", ")
+    )
+  }
+  settings_env$time_conversion <- factors
+  settings_env$default_time_scale <- default
 }
 
 
@@ -44,12 +65,13 @@ get_time_conversion <- function(factor = c("days", "weeks", "months", "years")) 
 #' Convert Time Values Using Scaling Factors
 #'
 #' @param times Numeric time values
-#' @param as A time scale to convert to
+#' @param as A time scale to convert to. One of "days", "weeks", "months", "years"
 #'
 #' @return Returns a numeric vector calculated from `times / get_time_conversion(factor = as)`
 
-get_time_as <- function(times, as = c("days", "weeks", "months", "years")) {
+get_time_as <- function(times, as = NULL) {
+  if (is.null(as)) as <- settings_env$default_time_scale
   if (!is.numeric(times)) stop("times arguments must be numeric")
-  as <- match.arg(as)
+  as <- match.arg(as, c("days", "weeks", "months", "years"))
   times / get_time_conversion(as)
 }
---
+++ tests/testthat/test-plot_km2.R
@@ -0,0 +1,80 @@
+test_that("kmplot2 works", {
+  skip_if_not_installed("vdiffr")
+
+  data(weighted_twt)
+  data(adtte_twt)
+  data(pseudo_ipd_twt)
+
+  # plot by trial
+  expect_warning(
+    vdiffr::expect_doppelganger(
+      title = "kmplot2_by_trial",
+      fig = function() {
+        kmplot2(
+          weights_object = weighted_twt,
+          tte_ipd = adtte_twt,
+          tte_pseudo_ipd = pseudo_ipd_twt,
+          trt_ipd = "A",
+          trt_agd = "B",
+          trt_common = "C",
+          trt_var_ipd = "ARM",
+          trt_var_agd = "ARM",
+          endpoint_name = "Overall Survival",
+          km_conf_type = "log-log",
+          km_layout = "by_trial",
+          time_scale = "month",
+          break_x_by = 2
+        )
+      }
+    ),
+    "select_"
+  )
+
+
+
+  # plot by arm
+  vdiffr::expect_doppelganger(
+    title = "kmplot2_by_am",
+    fig = function() {
+      kmplot2(
+        weights_object = weighted_twt,
+        tte_ipd = adtte_twt,
+        tte_pseudo_ipd = pseudo_ipd_twt,
+        trt_ipd = "A",
+        trt_agd = "B",
+        trt_common = "C",
+        trt_var_ipd = "ARM",
+        trt_var_agd = "ARM",
+        endpoint_name = "Overall Survival",
+        km_conf_type = "log-log",
+        km_layout = "by_arm",
+        time_scale = "month",
+        break_x_by = 2
+      )
+    }
+  )
+
+  # plot all
+  vdiffr::expect_doppelganger(
+    title = "kmplot2_all",
+    fig = function() {
+      kmplot2(
+        weights_object = weighted_twt,
+        tte_ipd = adtte_twt,
+        tte_pseudo_ipd = pseudo_ipd_twt,
+        trt_ipd = "A",
+        trt_agd = "B",
+        trt_common = "C",
+        trt_var_ipd = "ARM",
+        trt_var_agd = "ARM",
+        endpoint_name = "Overall Survival",
+        km_conf_type = "log-log",
+        km_layout = "all",
+        time_scale = "month",
+        break_x_by = 2,
+        xlim = c(0, 20),
+        show_risk_set = FALSE
+      )
+    }
+  )
+})
