61b24dc11450ed43703c3f7b5790f5a3cfea3033
jinjliu <jinjieliu75@gmail.com>
2025-09-01 10:49:39 -0400

    42 add forest plot (#198)
    * Adding maic_forest_plot function along with the test file.
    
    * Test case is working
    
    * Updated forest plot code to incorporate the reviewer suggested changes
    
    ---------
    
    Co-authored-by: Liu <jinjie.liu@daiichisankyo.com>
    Co-authored-by: github-actions <41898282+github-actions[bot]@users.noreply.github.com>
    Co-authored-by: 27856297+dependabot-preview[bot]@users.noreply.github.com <27856297+dependabot-preview[bot]@users.noreply.github.com>
    Co-authored-by: Isaac Gravestock <isaac.gravestock@roche.com>

---
+++ tests/testthat/test-maic_forest_plot.R
@@ -0,0 +1,45 @@
+test_that("maic_forest_plot works for TTE", {
+  data(centered_ipd_twt)
+  data(adtte_twt)
+  data(pseudo_ipd_twt)
+
+  centered_colnames <- c("AGE", "AGE_SQUARED", "SEX_MALE", "ECOG0", "SMOKE", "N_PR_THER_MEDIAN")
+  centered_colnames <- paste0(centered_colnames, "_CENTERED")
+
+  #### derive weights
+  weighted_data <- estimate_weights(
+    data = centered_ipd_twt,
+    centered_colnames = centered_colnames
+  )
+
+  # inferential result
+  result <- maic_anchored(
+    weights_object = weighted_data,
+    ipd = adtte_twt,
+    pseudo_ipd = pseudo_ipd_twt,
+    trt_ipd = "A",
+    trt_agd = "B",
+    trt_common = "C",
+    normalize_weight = FALSE,
+    endpoint_name = "Overall Survival",
+    endpoint_type = "tte",
+    eff_measure = "HR",
+    time_scale = "month",
+    km_conf_type = "log-log"
+  )
+
+  make_plot <- function() {
+    maic_forest_plot(
+      result,
+      xlim = c(0, 3.5),
+      reference_line = 1
+    )
+  }
+  expect_error(make_plot(), NA)
+
+  skip_if_not_installed("vdiffr")
+  vdiffr::expect_doppelganger(
+    title = "maic_forest_plot_tte",
+    fig = make_plot()
+  )
+})
